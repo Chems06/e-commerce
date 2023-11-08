2faa886ea009feb26a290eb98996601e3d9a5d97
jouaiti.chems <chemsjouaiti06@gmail.com>
2023-11-08 23:02:12 +0100

    Modification du visuel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { ProductComponent } from './product/product.component';
 import { UserComponent } from './user/user.component';
+import { OrderComponent } from './order/order.component';
 
 const routes: Routes = [
   {path: 'form', component: FormComponent },
   {path: 'product', component: ProductComponent },
-  {path: 'user', component:UserComponent}
+  {path: 'user', component:UserComponent},
+  {path: 'order', component:OrderComponent}
 ];
 
 @NgModule({
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { Orders } from '../orders.list';
+import { Order } from '../order';
+import { OrderService } from '../order.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-order',
+  selector: 'order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent {
+  orders: Observable<Orders> | undefined;
+  selectedOrder: Order | undefined;
 
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit(): void {
+
+    this.showOrders();
+  }
+
+  showOrders() {
+    this.orders = this.orderService.getOrders();
+  }
+
+  selectOrder(order: Order) {
+    this.selectedOrder = order;
+    console.log('Commande séléctionnée' + order.user)
+  }
 }
